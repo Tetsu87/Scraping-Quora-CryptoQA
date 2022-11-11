7aaa32b0e57fd87445eb525b294591e817a8f34e
Tetsuro Kai <tetsuro.kai87@gmail.com>
2022-11-10 23:31:01 -0800

    driver setting changed

--- connection_test.py
+++ connection_test.py
@@ -2,15 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument(" --window-size=1920x1080")
 
-driver = webdriver.Chrome(executable_path = './chromedriver3')
+driver = webdriver.Chrome(executable_path = './chromedriver',options=chrome_options)
 driver.implicitly_wait(10)
 driver.get(
     'https://www.library.chiyoda.tokyo.jp/'
 )
 
-schedule_el = driver.find_elements(By.CLASS_NAME, "schedule-list01__text")
-# schedule_el = driver.find_element_by_class_name("schedule-list01__text")
+# schedule_el = driver.find_elements(By.CLASS_NAME, "schedule-list01__text")
+schedule_el = driver.find_elements(By.XPATH, '//li[@id="chiyoda-today-status"]/div/div/span')
+
 
 print([s.text for s in schedule_el])
 
